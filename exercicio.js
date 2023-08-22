7417f4455f99492af8487d308f21d38a55a9447f
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-22 17:33:46 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,36 @@
+/* joão paulo
+
+*/
+
+/*
+
+* 1. sera impresso o valor que a pessoa executou no código
+* 2.ele ira executara algumas respotas diferentes e algumas parecidas
+
+* 3.o prompt serve para saber informações pessoais da pessoa como por exemplo nome,idade,cidade
+e etc.
+*/
+1
+let Nome;
+let idade;
+console.log (typeof Nome);
+console.log(typeof idade);
+/*
+
+porque são dados pessoais
+*/
+nome = prompt ("qual eo seu nome");
+idade = prompt ("qual sua idade");
+console.log ("ola , nome ","você tem"idade,"anos");
+
+2
+
+let joão;
+let não;
+let talvez;
+sim =prompt ("você tem uma cor favorita?");
+não =prompt ("você tem uma casa?");
+diariamente = prompt ("você trabalha?");
+console.log("você tem uma cor favorita?", sim);
+console.log("você tem uma renda mensal?",não);
+console.log("você tem uma casa");
